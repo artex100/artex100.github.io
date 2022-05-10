4a080b59e589869378ed7d6559e324c825d13527
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-10 23:41:52 +0300

    toimi

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $().ready(function () {
     $("#text").html("Text added by jQuery code.");
 });
+
+// Add "https://ipinfo.io" statement
+// this will communicate with the ipify servers
+// in order to retrieve the IP address
+$.get("https://ipinfo.io", function (response) {
+    alert(response.ip);
+}, "json")
