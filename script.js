ec99ab0d37a9545ebcc301f34c325431c88324b3
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-10 23:48:54 +0300

    sadasdasd

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-$().ready(function () {
-    $("#text").html("Text added by jQuery code.");
-});
-
-
-
 $.getJSON("https://api.ipify.org?format=json", function (data) {
 
     // Setting text of element P with id gfg
