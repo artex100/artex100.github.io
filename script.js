3e60e4be36b67ed74212619c11307ea8f12fb2a3
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-11 09:52:40 +0300

    maybe?

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$.getJSON("https://api.ipify.org?format=json", function (data) {
+$.getJSON('https://ipinfo.io/json', function(data) {
 
     // Setting text of element P with id gfg
     $("#gfg").html(data.ip);
 })
+
