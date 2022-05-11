e05e482d8a5373658636804f6a6aa19f25890157
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-11 10:29:53 +0300

    ip

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-$.getJSON('https://jsonip.com/', function(data) {
+/*$.getJSON('https://jsonip.com/', function (data) {
 
     // Setting text of element P with id gfg
     $("#gfg").html(data.ip);
-})
+});
+**/
 
+
+var VisitorAPI = function (t, e, a) { var s = new XMLHttpRequest; s.onreadystatechange = function () { var t; s.readyState === XMLHttpRequest.DONE && (200 === (t = JSON.parse(s.responseText)).status ? e(t.data) : a(t.status, t.result)) }, s.open("GET", "https://visitorapi-dev.uc.r.appspot.com/api/?pid=" + t), s.send(null) };
+new VisitorAPI(projectID, successHandler, errorHandler);
+
+VisitorAPI(
+    "om61tWZOjuBBPxTdDlpy",
+    function (data) { console.log(data) },
+    function (errorCode, errorMessage) { console.log(errorCode, errorMessage) }
+    $("#gfg").html(data.ipAddress)
+);
