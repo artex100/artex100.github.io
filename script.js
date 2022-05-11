65faf719ca76808adaa54ba437fabcf87f0b80ec
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-11 11:41:29 +0300

    hemmeti ip

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-$.getJSON('https://api.ipregistry.co/?key=tryout', function (data) {
+let apiKey = '25864308b6a77fd90f8bf04b3021a48c1f2fb302a676dd3809054bc1b07f5b42';
+
+$.getJSON('http://ip.jsontest.com/', function (data) {
+    console.log(JSON.stringify(data, null, 2))
 
     // Setting text of element P with id gfg
     $("#gfg").html(data.ip);
-});
-
-$ip = $_SERVER['REMOTE_ADDR'] {
-    console.log()
-};
+})
