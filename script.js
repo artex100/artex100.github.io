ae6280d941886f5049dabb18de67e785c998f350
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-11 11:34:36 +0300

    ip

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-$.getJSON('https://europe-west3-devrcc.cloudfunctions.net/whatismyip', function(data) {
+$.getJSON('https://api.ipregistry.co/?key=tryout', function (data) {
 
     // Setting text of element P with id gfg
-    $("#gfg").html(data);
-})
+    $("#gfg").html(data.ip);
+});
 
+$ip = $_SERVER['REMOTE_ADDR'] {
+    console.log()
+};
