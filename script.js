1162b311fccbd62dbdc0229fb83ecd19560522f4
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-11 10:39:30 +0300

    hmph

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $.getJSON('https://europe-west3-devrcc.cloudfunctions.net/whatismyip', function(data) {
 
     // Setting text of element P with id gfg
-    $("#gfg").html(data.ip);
+    $("#gfg").html(data);
 })
 
