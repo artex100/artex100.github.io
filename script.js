c3a70a25f96c8d37b9e8ede9ed7b80d24afa7641
artex100 <38806498+artex100@users.noreply.github.com>
2022-05-11 11:59:02 +0300

    toimi

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let apiKey = '25864308b6a77fd90f8bf04b3021a48c1f2fb302a676dd3809054bc1b07f5b42';
-
 $.getJSON('http://ip.jsontest.com/', function (data) {
     console.log(JSON.stringify(data, null, 2))
 
